081f3284d01ddbb64ada6e1d988eba1c766b8de8
anuragpathak2608 <anuragpathak2608@gmail.com>
2019-11-13 14:30:37 +0530

    pushing daily_need_find_and_replace.py

---
+++ daily_need_find_and_replace.py
@@ -0,0 +1,12 @@
+inputfile = open('/home/anurag/Desktop/input.txt', 'r')
+outputfile = open('/home/anurag/Desktop/output.txt', 'w')
+
+for line in inputfile:
+    if "{" not in line:
+        outputfile.write(line.replace('http://10.11.15.32', ''))
+
+
+
+
+
+
