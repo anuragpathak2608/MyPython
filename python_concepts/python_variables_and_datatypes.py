a3df35d738925cc323d46741e09b62674e1e3179
anuragpathak2608 <anuragpathak2608@gmail.com>
2019-11-22 21:14:49 +0530

    added few lines of code to python variables file

--- python_concepts/python_variables_and_datatypes.py
+++ python_concepts/python_variables_and_datatypes.py
@@ -16,3 +16,36 @@ p, q, r = 1, 1.1, 'string'
 print(p, q, r)
 
   #print complete string from the index
+
+#Global and local variable
+var1 = "Global"
+
+
+def func1():
+    var1 = "Local"
+    print("this is  local variable", var1)
+
+
+def func2():
+    print("this is global variable", var1)
+
+
+func1()
+
+func2()
+
+#Global and local variable
+
+
+def func3():
+    global var2
+    var2 = "Anurag"
+    print("Inside the function func3", var2)
+
+
+def func4():
+    print("Inside function func4", var2)
+
+
+func3()
+func4()
